4eef2a5ad390ae6a9912692a42730337a3da2747
aqmal28 <aqmalghifaliyanazhar@gmail.com>
2025-04-10 21:16:31 +0700

    init

---
+++ kuliah/text.php
@@ -0,0 +1,20 @@
+<?php
+
+$mb = "aqmal gebdut";
+
+echo $mb
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tes</title>
+</head>
+<body>
+    <h1>AQMAL BAU</h1>
+</body>
+</html>
